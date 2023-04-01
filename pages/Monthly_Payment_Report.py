312a7bcc46b47972a7275ff34e0fa49cb800e8f5
Nared7 <70996097+Nared7@users.noreply.github.com>
2023-04-01 12:17:43 +0630

    Add files via upload

--- pages/Monthly_Payment_Report.py
+++ pages/Monthly_Payment_Report.py
@@ -6,22 +6,38 @@ import json
 import gspread
 import pandas as pd
 
-# studentDataWB = xl.load_workbook(r'D:\Python_Projects\TrBrittneyInvoice\studentCost.xlsx')
-# invoiceDataWB = xl.load_workbook(r'D:\Python_Projects\TrBrittneyInvoice\InvoiceList_Jan2023.xlsx')
+def getInvoiceList():
+    # Request All Google Sheet Data to save request limit
+    # From this nested dictionary, I will extract the data I need.
+    cred_file = r"trbrittneystudentdata-bb3d38df148f.json"
+    gc = gspread.service_account(filename=cred_file)
+    invoiceWS_List = [sh.title for sh in gc.openall()]
+    with open("invoiceList.json", "w") as write_file:
+        json.dump(invoiceWS_List, write_file, indent=4, separators=(", ", ": "), sort_keys=True)  # encode dict into JSON
 
-# cred_file = r"pages/"
-cred_file = r"trbrittneystudentdata-bb3d38df148f.json"
-gc = gspread.service_account(filename=cred_file)
-database = gc.open('studentData')
-invoiceData = gc.open('InvoiceList_March2023')
+info_data = st.empty()
+info_data.write('Load Data from local Database')
+
+try:
+    with open('invoiceList.json') as json_file:
+        invoiceWS_List = json.load(json_file)
+except:
+    info_data.write("No Invoice List found. Press 'Fetch Invoice List' to update the database")
+    invoiceWS_List = []
 
 # info_data = 'Load Data from local Database'
-info_data = st.info('Load Data from local Database')
+invoice_option = st.selectbox('Which month would you like to review?', invoiceWS_List)
+
+
 with open('studentData.json') as json_file:
     allStData = json.load(json_file)
-
-with open('invoiceData.json') as json_file:
-    invoice_data = json.load(json_file)
+try:
+    with open(invoice_option + '.json') as json_file:
+        invoice_data = json.load(json_file)
+        info_data.write('Local Data loaded from ' + invoice_option)
+except:
+    info_data.write('No such Invoice Data. Press Fetch to update the database')
+    invoice_data = {}
 
 studentData = {}
 invoiceData = {}
@@ -32,6 +48,52 @@ dbStudentList = []
 inStudentList = []
 focStudentList = []
 i = 0
+
+
+def fetchData():
+    global allStData, invoice_data
+    cred_file = r"trbrittneystudentdata-bb3d38df148f.json"
+    gc = gspread.service_account(filename=cred_file)
+    database = gc.open('studentData')
+    invoiceWS = gc.open(invoice_option)
+    ws_list = database.worksheets()
+    invoice_ws = invoiceWS.worksheets()
+
+    for sheet in ws_list:
+        list_of_dicts = sheet.get_all_records()
+
+        #       Remove whitespace the data
+        for i in range(len(list_of_dicts)):
+            list_of_dicts[i] = {x.strip(): v for x, v in list_of_dicts[i].items()}
+
+        allStData[sheet.title] = list_of_dicts
+
+    for sheet in invoice_ws:
+        list_of_dicts = sheet.get_all_records()
+
+        #       Remove whitespace the data
+        for i in range(len(list_of_dicts)):
+            list_of_dicts[i] = {x.strip(): v for x, v in list_of_dicts[i].items()}
+
+        invoice_data[sheet.title] = list_of_dicts
+
+
+    with open("studentData.json", "w") as write_file:
+        json.dump(allStData, write_file, indent=4, separators=(", ", ": "), sort_keys=True)  # encode dict into JSON
+
+    with open(invoice_option + '.json', "w") as write_file:
+        json.dump(invoice_data, write_file, indent=4, separators=(", ", ": "), sort_keys=True)  # encode dict into JSON
+
+    with open('studentData.json') as json_file:
+        allStData = json.load(json_file)
+
+    with open(invoice_option + '.json') as json_file:
+        invoice_data = json.load(json_file)
+        info_data.write('Local Data loaded from ' + invoice_option)
+
+
+st.button('Fetch Data', on_click=fetchData)
+st.button('Fetch Invoice List', on_click=getInvoiceList)
 
 studentData.clear()
 for database_className, database_students in allStData.items():
@@ -66,6 +128,7 @@ for className in studentData:
     totalStudent = studentData.get(className)
     focStudent = focStudentData.get(className)
     unpaidStudent = []
+    print(paidStudent, className)
 
     for ss_foc in focStudent:
         try:
